85a003e3d9ac3bc27b928e0b660ac52fa3811da1
ufuomasamson <samsonufuomaenzo@gmail.com>
2025-05-29 16:56:39 -0700

    fix: create separate admin app build

---
+++ client/admin/vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+
+export default defineConfig({
+  plugins: [react()],
+  root: resolve(__dirname),
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
+  },
+  server: {
+    port: 5174
+  },
+  base: '/',
+  publicDir: 'public'
+}) 
